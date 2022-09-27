0669bc1b7fa03e3deeb25055f18940c800eda384
Maithili Kharabe <62667818+MRK04@users.noreply.github.com>
2022-09-27 20:29:49 +0530

    IMP

---
+++ Basic Join/Top Competitors.sql
@@ -0,0 +1,8 @@
+SELECT Submissions.hacker_id, Hackers.name from Hackers, Submissions, Challenges, Difficulty
+WHERE Submissions.score = Difficulty.score
+AND Hackers.hacker_id = Submissions.hacker_id 
+AND Submissions.challenge_id = Challenges.challenge_id 
+AND Challenges.difficulty_level = Difficulty.difficulty_level
+GROUP BY Submissions.hacker_id, Hackers.name
+HAVING count(*) > 1 
+ORDER BY count(*) DESC, Submissions.hacker_id ASC;
